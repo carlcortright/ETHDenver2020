bfe1a99d2ac6d6c0d86fc38b3a762e28cd123654
justsomeguy <thenaders3@gmail.com>
2020-02-15 10:59:25 -0800

    started working on deployjs

--- www/src/ethereum/deploy.js
+++ www/src/ethereum/deploy.js
@@ -1,14 +1,30 @@
-function deploy(
-    name,
-    symbol,
-    decimals,
-    fundraiseAmount,
-    interestRate,
-    endTime,
-    description,
-    addressUSDC,
-    recipient,
-) {
-    console.log("deploying")
-    return
+import { getWeb3, getAddress } from './ethereum';
+import SponsorToken from '../abis/SponsorToken.json';
+
+export const deployContract = async (argumentObj) => {
+    const web3 = await getWeb3();
+    const from = getAddress();
+    // const gas
+    const data = SponsorToken.bytecode;
+    const abi = SponsorToken.abi;
+    const SponsorContract = new web3.eth.Contract(abi, null, { data });
+
+    const sponsor = await SponsorContract.deploy(argumentObj).send({ from });
+
+    console.log(sponsor.options.address);
 }
+
+// function deploy(
+//     name,
+//     symbol,
+//     decimals,
+//     fundraiseAmount,
+//     interestRate,
+//     endTime,
+//     description,
+//     addressUSDC,
+//     recipient,
+// ) {
+//     console.log("deploying")
+//     return
+// }
