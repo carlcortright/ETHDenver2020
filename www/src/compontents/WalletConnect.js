76064e6778c753e44b094adfa56b90260f874593
justsomeguy <thenaders3@gmail.com>
2020-02-15 09:55:52 -0800

    added connect functions and ethereumjs file

--- www/src/compontents/WalletConnect.js
+++ www/src/compontents/WalletConnect.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 import { Button } from 'rebass';
 
 class WalletConnect extends Component {
+	constructor() {
+		super();
+		this.state = {
+			walletConnected: false,
+			userAddress: ''
+		}
+	}
+
+	connnectWallet = async () => {
+		if(!window.ethereum) {
+			window.alert('No Ethereum wallet found');
+		}
+		else {
+			const accounts = await window.ethereum.enable();
+			const userAddress = accounts[0];
+			this.setState({ userAddress, walletConnected: true });
+		}
+	}
+
 	render() {
+		const { userAddress, walletConnected } = this.state;
 		return (
-            <Button variant='primary' fontWeight={500} mr={4}>Connect Wallet</Button>
+			<div>
+				{!walletConnected
+				?
+				<Button variant='primary' fontWeight={500} mr={4} onClick={this.connnectWallet}>Connect Wallet</Button>
+				:
+				<div>{userAddress}</div>
+				}
+			</div>
 		);
 	}
 }
---
+++ www/src/ethereum/ethereum.js
@@ -0,0 +1,27 @@
+import Web3 from 'web3';
+import BigNumber from 'bignumber.js';
+
+let web3;
+
+export const getWeb3 = async () => {
+    if(!web3) {
+        web3 = new Web3(Web3.givenProvider);
+    }
+    return web3;
+}
+
+export const getAddress = async () => {
+    const web3 = getWeb3();
+    const accounts = await web3.eth.getAccounts();
+    return accounts[0];
+}
+
+
+BigNumber.config({ DECIMAL_PLACES: 2 });
+
+export const formatTokenValue = async (value, tokenContract) => {
+    const decimals = await tokenContract.methods.decimals().call()
+    const bn = new BigNumber(value);
+    
+    return bn.shiftedBy(-decimals).toString(10);
+};
