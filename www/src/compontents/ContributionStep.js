d1947570904f6130778d0c47bd545e116e3d1cb9
Carl Cortright <carl@carlcortright.com>
2020-02-15 15:11:17 -0700

    update fundraise page

--- www/src/compontents/ContributionStep.js
+++ www/src/compontents/ContributionStep.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
-import { Flex, Heading, Image } from 'rebass';
+import { Flex, Heading, Image, Box, Text, Button } from 'rebass';
 import styled from 'styled-components';
 import USDC from '../img/usdc.svg';
+
+import Countdown from "react-countdown-now";
+
+import {
+    Label,
+    Input,
+    Select,
+    Textarea,
+    Radio,
+    Checkbox,
+  } from '@rebass/forms'
 
 class ContributionStep extends Component {
 	constructor(props) {
@@ -22,6 +33,47 @@ class ContributionStep extends Component {
                     <Flex alignItems='center'>
                         <Image mr={1} src={USDC} height='32px'/>
                         <Heading fontSize={4, 5}>Contribute to Fundraiser</Heading>
+                        <Box mx='auto' />
+                        <Heading fontSize={2, 3}>
+                            Time Remaining: 
+                            <Countdown date={Date.now() + 1000000} renderer={({ hours, minutes, seconds }) => <span> {hours}:{minutes}:{seconds}</span>}/>
+                        </Heading>
+                    </Flex>
+
+                    <Flex py={4}>
+                        <Flex width={1/2} flexDirection='column'>
+                            <Text fontSize={[ 2, 3 ]} my={2}>Name: {}</Text>
+                            <Text fontSize={[ 2, 3 ]} my={2}>Symbol: {}</Text>
+                            <Text fontSize={[ 2, 3 ]} my={2}>Target Amount: {}</Text>
+                            <Text fontSize={[ 2, 3 ]} my={2}>Interest Rate: {}</Text>
+                            <Text fontSize={[ 2, 3 ]} my={2}>Description: {}</Text>
+                        </Flex>
+                        <Flex width={1/2}>
+                            <Flex
+                                as='form'
+                                onSubmit={e => e.preventDefault()}
+                                width={1}
+                                justifyContent='center' 
+                                alignItems='center'>
+                                    <Box p={10}>
+                                        <Label htmlFor='amount'>Contribution Amount ($)</Label>
+                                        <Input
+                                        id='amount'
+                                        name='amount'
+                                        placeholder='0'
+                                        type="number" 
+                                        min="0.01" 
+                                        step="0.01" 
+                                        />
+                                    </Box>
+
+                                    <Box p={10} >
+                                        <Button fontSize={3} >
+                                        Contribute
+                                        </Button>
+                                    </Box>
+                            </Flex>
+                        </Flex>
                     </Flex>
                     
             </Flex>
